214d6a9b339640225d50da890df6720385c75c49
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2020-11-07 20:08:38 +0200

    Addes working BFS/DFS

--- crawler.py
+++ crawler.py
@@ -5,7 +5,8 @@ import re
 
 class Crawler:
 
-
+    visited = []
+    queue = []
 
     #Constructor
     def __init__(self, url, n, threads, warm_start, algo):
@@ -16,34 +17,52 @@ class Crawler:
         self.algo = algo
 
     def initializeCrawl(self):
-
         if not self.warm_start:
-            self.visited.clear();
-
-        with urllib.request.urlopen(self.url) as response:
-            html = response.read().decode('utf-8')
-            links = self.findLinks(html)
-        print(*links,sep = '\n')
+            self.visited.clear()
+        
+        links = self.generateUrl(self.url)
         self.visited.append(self.url)
-        self.queue.append(self.url)
+        for link in links:
+            self.queue.append(link)
 
         if self.algo == 'BFS':
-            self.crawlBFS(links);
+            self.crawlBFS(self.queue)
 
         if self.algo == 'DFS':
-            self.crawlDFS(links);
+            self.crawlDFS(self.queue)
+
+        print("Visited links:")
+        print(self.visited)
 
     #Crawling using Breadth First Search of links
-    def crawlBFS(self, links):
-        for link in links[0:self.n+1]:
-            pass
+    def crawlBFS(self, queue):
+        for url in queue:
+            if len(self.visited) >= self.n:
+                break
+            self.visited.append(url)
+            links = self.generateUrl(url)
+            for link in links:
+                queue.append(link)
 
     #Crawling using Depth First Search of links
-    def crawlDFS(self, links):
-        pass
+    def crawlDFS(self, queue):
+        for url in queue:
+            if len(self.visited) >= self.n:
+                break
+            self.visited.append(url)
+            links = self.generateUrl(url)
+            for link in links:
+                queue.insert(0,link)
 
     #Using regular expression to find all links in a single link's source file.
     def findLinks(self, link):
         pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  #regex for href=
         return re.findall(pattern, link)
+
+    #Function generating all links from a given url
+    def generateUrl(self,url):
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
+            links = self.findLinks(html)
+        return links
 
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import threading
 
 
 if __name__ == '__main__':
-    r1 = Crawler('https://www.instagram.com/',10,2,False,'BFS')
+    r1 = Crawler('https://www.instagram.com/',10,2,False,'DFS')
     print("Hello!")
     r1.initializeCrawl()
