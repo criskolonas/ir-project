0bfa1b64d2566a0f569b13bba82c4f6f2f8ad1ed
Christos Kolonas <ckolonas@csd.auth.gr>
2020-11-07 23:30:42 +0200

    commit

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ import threading
 
 
 if __name__ == '__main__':
-    r1 = Crawler('https://www.instagram.com/',10,2,False,'DFS')
-    print("Hello!")
+    r1 = Crawler('https://www.roh.gr/',5,2,False,'DFS')
     r1.initializeCrawl()
