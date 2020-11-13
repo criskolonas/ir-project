99c66bc4cca4efb504fb87d761ac451d38fb7a40
mitsos-tsali <dtsaligo@csd.auth.gr>
2020-11-13 21:56:01 +0200

    Changed "Crawler" parameters

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from crawler import Crawler
-import threading
 
 
 if __name__ == '__main__':
-    r1 = Crawler('https://www.roh.gr/',5,2,False,'DFS')
+    r1 = Crawler('https://www.instagram.com/',30,3,False,'DFS')
     r1.initializeCrawl()
