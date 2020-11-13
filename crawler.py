75369f6054a3bef334451d5d460ddc7b63a911e9
mitsos-tsali <dtsaligo@csd.auth.gr>
2020-11-13 21:55:43 +0200

    Added multi threading Added "soups" list Simplified whole class

--- crawler.py
+++ crawler.py
@@ -1,15 +1,16 @@
-import urllib.request
-import urllib.response
-import re
+from urllib import request
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import re
+import time
+
 
 class Crawler:
-
     queue = []
     visited = []
+    soups = []
 
-
-    #Constructor
+    # Constructor
     def __init__(self, url, n, threads, warm_start, algo):
         self.url = url
         self.n = n
@@ -17,65 +18,49 @@ class Crawler:
         self.warm_start = warm_start
         self.algo = algo
 
+    # Crawler initializer
     def initializeCrawl(self):
+        t1 = time.perf_counter()
         if not self.warm_start:
             self.visited.clear()
-        
-        links = self.generateUrl(self.url)
+            self.queue.clear()
+
         self.visited.append(self.url)
-        self.parseHTML(self.url)
-        for link in links:
-            self.queue.append(link)
+        self.queue = self.getLinks(self.url)
 
-        if self.algo == 'BFS':
-            self.crawlBFS(self.queue)
+        with ThreadPoolExecutor(max_workers=self.threads) as executor:
+            executor.map(self.crawlStart, self.queue)
+        t2 = time.perf_counter()
+        print(t2 - t1)
+        print(len(self.visited))
 
-        if self.algo == 'DFS':
-            self.crawlDFS(self.queue)
-
-        print(self.visited)
-        print(self.queue)
-
-
-    #Crawling using Breadth First Search of links
-    def crawlBFS(self, queue):
-        for url in queue:
-            if len(self.visited) >= self.n:
-                break
-            self.visited.append(url)
-            links = self.generateUrl(url)
-            for link in links:
-                queue.append(link)
-
-    #Crawling using Depth First Search of links
-    def crawlDFS(self, queue):
-        for url in queue:
-            if len(self.visited) >= self.n:
-                break
-            self.visited.append(url)
-            self.parseHTML(url)
-
-
-            links = self.generateUrl(url)
-            for link in links:
-                queue.insert(0, link)
-
-    #Using regular expression to find all links in a single link's source file.
-    def findLinks(self, link):
-        pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  #regex for href=
-        return re.findall(pattern, link)
-
-    #Function generating all links from a given url
-    def generateUrl(self,url):
-        with urllib.request.urlopen(url) as response:
-            html = response.read().decode('utf-8')
-            links = self.findLinks(html)
-        return links
-
-    def parseHTML(self, link):
-        with urllib.request.urlopen(link) as response:
-            linkHTML = BeautifulSoup(response, 'html.parser')
+    # Method to generate links from given url and create "soup" objects for each link
+    def getLinks(self, url):
+        with request.urlopen(url) as response:
             try:
-                print(linkHTML.html.title)
-            except AttributeError:
-                print(link)
+                html = response.read().decode('utf-8')
+                soup = BeautifulSoup(html,"html.parser")
+                self.soups.append(soup.prettify())
+                pattern = re.compile(
+                    'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+                links = re.findall(pattern, html)
+                links_clean = list(dict.fromkeys(links))
+                return links_clean
+            except Exception as exc:
+                print(exc)
+
+    # Crawling using Selected Algorithm
+    def crawlStart(self, url):
+        if len(self.visited) >= self.n:
+            return 0
+        if url in self.visited:
+            self.queue.pop(0)
+        else:
+            self.visited.append(url)
+            links = self.getLinks(url)
+            if self.algo == "DFS":
+                for link in links:
+                    self.queue.insert(0, link)
+            elif self.algo == "BFS":
+                for link in links:
+                    self.queue.append(link)
