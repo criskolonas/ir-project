983b875d9c2861298c4cbd23f6e49593ae95001d
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2020-12-12 15:43:57 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from crawler import Crawler
 
-
 if __name__ == '__main__':
-    r1 = Crawler('https://www.instagram.com/',30,3,False,'DFS')
+    r1 = Crawler('https://www.in.gr/', 200, 5, False, 'BFS')
     r1.initializeCrawl()
