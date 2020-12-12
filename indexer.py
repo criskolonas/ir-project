362247f31beec9727a43bccbcd88a802ed636649
Chris Kolonas <ckolonas@csd.auth.gr>
2020-12-12 20:14:16 +0200

    more

--- indexer.py
+++ indexer.py
@@ -1,21 +1,36 @@
-from bs4 import BeautifulSoup
 import re
-
+from bs4 import BeautifulSoup
+from urllib import request
 
 class Indexer:
-    inverted_index = []
+    documents = []
+    tokenized_docs = []
+    inverted_index = {}
+
+    def __init__(self,soup_objects):
+        self.soup_objects = soup_objects
+
+    def extract_text(self):
+        for s_obj in self.soup_objects:
+            s_obj.find_all('p')
+            self.documents.append(s_obj.text)
+
+    def create_indexer(self):
+
+
+    def tokenize_documents(self):
+        for doc in self.documents:
+            doc = self.string_cleanup(doc)
+            self.tokenized_docs.append(doc.split())
 
     def string_cleanup(self,doc):
         doc_lowercase = doc.lower()
         doc_no_punctuation = s = re.sub(r'[^\w\s]', '', doc_lowercase)
         return doc_no_punctuation
 
-    def create_word_dictionary(self, documents):
-        dictionary = list()
-        for doc in documents:
-            doc = self.string_cleanup(doc)
-            doc_words = doc.split()
-            for word in doc_words:
-                dictionary.append(word)
-        dictionary = list(set(dictionary))
-        print(dictionary)
+    def update_indexer(self):
+
+
+if __name__ == '__main__':
+    inde = Indexer()
+
