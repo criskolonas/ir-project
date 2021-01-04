34d7c0156fbe708cd1c27166613bedb0d2ec4d34
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2021-01-04 18:01:11 +0200

    Implemented main method

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from crawler import Crawler
+from indexer import Indexer
 
 if __name__ == '__main__':
-    r1 = Crawler('https://www.in.gr/', 200, 5, False, 'BFS')
+    r1 = Crawler('https://www.in.gr/', 10, 5, False, 'BFS')
     r1.initializeCrawl()
+    ind = Indexer(r1.soups)
+    rev_ind = ind.create_indexer()
+
