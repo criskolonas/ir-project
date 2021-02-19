dc55551cad2a1ae6eae8c5f9ae70a85fa7bf4879
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2021-02-19 23:18:21 +0200

    Almost ready crawler

--- crawler.py
+++ crawler.py
@@ -5,9 +5,10 @@ import time
 from document import Document
 from concurrent.futures import ThreadPoolExecutor
 
+"""Method to find link format using regex and to delete the duplicate links
+Returns clean links"""
 
-# Method to find link format using regex and to delete the duplicate links
-# Returns clean links
+
 def remove_duplicates(links):
     links_clean = []
     for link in links:
@@ -18,13 +19,7 @@ def remove_duplicates(links):
     return links_clean
 
 
-def check_if_link_in_file(link):
-    with open("visited.txt") as visited_txt:
-        lines = [line.rstrip() for line in visited_txt]
-        for line in lines:
-            if link == line:
-                return True
-    return False
+"""Function that makes a request from a url"""
 
 
 def requestUrl(url):
@@ -36,12 +31,24 @@ def requestUrl(url):
         return None
 
 
+"""Function that finds all text in paragraphs from soup object"""
+
+
+def parseInfo(soup):
+    texts = ''
+    for p in soup.findAll('p'):
+        texts += p.text
+    return texts
+
+
 class Crawler:
     queue = []
     visited = []
     documents = []
+    new_size_visited = 0
+    new_size_queue = 0
 
-    # Constructor
+    """Constructor"""
     def __init__(self, url, n, threads, warm_start, algo):
         self.url = url
         self.n = n
@@ -49,67 +56,98 @@ class Crawler:
         self.warm_start = warm_start
         self.algo = algo
 
-    # Crawler1 initializer
+    """Crawler initializer"""
     def initializeCrawl(self):
-
         t1 = time.perf_counter()
-        if self.warm_start:
-            visited_txt = open("visited.txt", "w").close()
-            self.visited.clear()
-            self.queue.clear()
-
-        self.queue.append(self.url)
-        self.crawl(self.queue[0])
+        self.warmStartCheck()
+        self.crawl(self.url)
+        self.write_file()
         t2 = time.perf_counter()
-        print("Time Elapsed: " + str(t2 - t1) + "s." + "\n-----------------")
-        print(len(self.visited) == self.n)
+        print("Time Elapsed: {} s.".format(t2 - t1))
+        print(len(self.visited) == (self.n + self.new_size_visited))
 
-    # Crawl method
+    """Crawl method"""
     def crawl(self, url):
-        if len(self.visited) >= self.n:
-            return
-        if url in self.visited:
-            self.queue.pop(0)
-            self.crawl(self.queue[0])
-        else:
-            self.visited.append(url)
-            # with ThreadPoolExecutor(max_workers=self.threads) as executor:
-            #     print("Creating task!")
-            #     task = executor.submit(requestUrl, url)
-            #     print("Adding callback!")
-            #     task.add_done_callback(self.callbackContinue)
-            #     print("Callback added!")
-            self.callbackContinue(url)
-            self.crawl(self.queue[0])
+        while len(self.visited) < (self.n + self.new_size_visited):
+            if url in self.visited:
+                self.queue.pop(0)
+            else:
+                # print("Crawling url: {}".format(url))
+                self.visited.append(url)
+                with ThreadPoolExecutor(max_workers=self.threads) as executor:
+                    task = executor.submit(requestUrl, url)
+                    task.add_done_callback(self.callbackContinue)
+            url = self.queue[0]
 
+    """The callback function - general function method of the threadpool executor"""
     def callbackContinue(self, url):
         try:
-            links = []
-            texts = ''
-            result = requestUrl(url)
+            result = url.result()
+            soup = BeautifulSoup(result.content, "lxml")
             if result.status_code == 200:
-                soup = BeautifulSoup(result.content, "lxml")
-                for link in soup.find_all('a', href=True):
-                    links.append(link.get("href"))
-                links = remove_duplicates(links)
-                for p in soup.findAll('p'):
-                    texts += p.text + '\n'
-                doc = Document(url, texts)
+                doc = Document(url, parseInfo(soup))
                 self.documents.append(doc)
-                if self.algo == "DFS":
-                    for link in links:
-                        self.queue.insert(0, link)
-                elif self.algo == "BFS":
-                    for link in links:
-                        self.queue.append(link)
+                self.findLinks(soup)
+                self.queue.pop(0)
+            else:
+                self.findLinks(soup)
+                self.queue.pop(0)
         except Exception as ex:
             print(url.url + str(ex))
         except UnicodeEncodeError as er:
             print(url.url + str(er))
 
-    # def write_file(self):
-    #     # if dont exist creates and appends too
-    #     visited_txt = open("visited.txt", "a+")
-    #     for link in self.visited:
-    #         visited_txt.write(link + "\n")
-    #     visited_txt.close()
+    """Method that finds links from given file, and adds them to 
+    visited or queue list respectively"""
+    def findLinks(self, soup):
+        links = []
+        for link in soup.find_all('a', href=True):
+            links.append(link.get("href"))
+        links = remove_duplicates(links)
+        if self.algo == "DFS":
+            for link in links:
+                self.queue.insert(0, link)
+        elif self.algo == "BFS":
+            for link in links:
+                self.queue.append(link)
+
+    """Check for warm_start variable.
+    If True, erase both .txt files and start crawling from begining
+    If False, continue from the first line-link in the queue.txt until n number of crawls. """
+    def warmStartCheck(self):
+        if self.warm_start:
+            visited_txt = open("visited.txt", "w").close()
+            queue_txt = open("queue.txt", "w").close()
+            self.visited.clear()
+            self.queue.clear()
+        else:
+            with open("visited.txt", "r") as visited_txt:
+                with open("queue.txt", "r") as queue_txt:
+                    txt_links = visited_txt.readlines()
+                    if len(txt_links) == 0:
+                        print("File is Empty, thus -False- parameter is not allowed.\nPlease try again!")
+                        exit(3)
+                    for line in txt_links:
+                        line = re.sub("\\n+", "", line)
+                        self.visited.append(line)
+                    queue_links = queue_txt.readlines()
+                    if len(queue_links) == 0:
+                        print("Queue is Empty, thus -False- parameter is not allowed.\nPlease try again!")
+                        exit(4)
+                    for line in queue_links:
+                        line = re.sub("\\n+", "", line)
+                        self.queue.append(line)
+            self.url = self.queue[0]
+            self.new_size_visited = len(self.visited)
+
+    """Method that writes to files"""
+    def write_file(self):
+        # if dont exist creates and appends too
+        visited_txt = open("visited.txt", "a+")
+        queue_txt = open("queue.txt", "a+")
+        counter_visited = self.new_size_visited
+        counter_queue = self.new_size_queue
+        for i in range(counter_visited, len(self.visited)):
+            visited_txt.write(self.visited[i] + "\n")
+        for i in range(counter_queue, len(self.queue)):
+            queue_txt.write(self.queue[i] + "\n")
