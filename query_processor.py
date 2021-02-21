a16c70e2bb3cec8e8a883df91bad258b78c6b849
Christos Kolonas <ckolonas@csd.auth.gr>
2021-02-21 15:46:00 +0200

    almost ready qp

--- query_processor.py
+++ query_processor.py
@@ -1,21 +1,30 @@
+import math
+
 import numpy as np
 
 
 class QuerryProcessor:
 
-    def __init__(self, query, indexer, d_n):
+    def __init__(self, indexer, d_n):
         self.d_n = d_n
-        self.query = query
         self.inverted_index = indexer
 
-    # creates and returns a numpy vector for the query made
-    # the query is the last document of the indexer, meaning the last appearance of every term in the indexer dictionary
-    def get_query_vector(self):
-        tfid_list = []
-        for term in self.inverted_index.keys():
-            tfid_list.append(self.inverted_index[term][self.d_n - 1].score)
-        return np.array(tfid_list)
-
+    def calculate_cosine_similarity(self,vector1,vector2):
+        dot_product = sum(p * q for p, q in zip(vector1, vector2))
+        magnitude = math.sqrt(sum([val ** 2 for val in vector1])) * math.sqrt(sum([val ** 2 for val in vector2]))
+        if not magnitude:
+            return 0
+        return dot_product / magnitude
     # calculates the cosine similarity and return a list of top-k documents closest to the given query
-    def calculate_cosine_similarity(self, k, query_vector):
+    def compare_documents(self):
         top_k_docs = []
+        tfid_query_list = []
+        for term in self.inverted_index.keys():
+            tfid_query_list.append(self.inverted_index[term][self.d_n-1].score)
+        query_vector = np.array(tfid_query_list)
+        for i in range(self.d_n-1):
+            tfid_doc_list = []
+            for term in self.inverted_index.keys():
+                tfid_doc_list.append(self.inverted_index[term][i].score)
+            doc_vector = np.array(tfid_doc_list)
+            print(self.calculate_cosine_similarity(query_vector,doc_vector))
